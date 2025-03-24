4cebb03591739745f54200dc43b5e4e728ca6b20
awanishkrai <awanishrai420@gmail.com>
2025-03-25 00:12:44 +0530

    frontend added

--- booking_system/booking_system/settings.py
+++ booking_system/booking_system/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-bgrghj5c1ce+pt=ufehckb8@$j-gv8$#fd#cyv**1$%%(@d!b@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', '192.168.162.159']
+ALLOWED_HOSTS = ['192.168.52.159', 'localhost', '127.0.0.1']
+
 
 
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'bookings', 
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'booking_system.urls'
@@ -124,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+     "http://127.0.0.1:5500", 
+    # add any other domains that you want to allow
+]
